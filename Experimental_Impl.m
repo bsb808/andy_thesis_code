72639bb5f686e6484cf1b03075698f6c9add9fdd
Schnieders, Joseph A <jaschnie@nps.edu>
2018-01-25 11:57:03 -0800

    Tank Testing Day 1

--- Experimental_Impl.m
+++ Experimental_Impl.m
@@ -20,20 +20,23 @@ data = csvread('tcurve_nohead.csv');
 breakpoints1=data(end:-1:1,1)';
 table_data = data(end:-1:1,2)';
 
+
 %%
 % Variable and Constant Initiation and Definition
 % Step Inputs
+kfyaw=zeros(50);
 vo = 0.0; vf = 0.0; % Initial and Final Speed [m/s]
-yo = 0; yf=pi()/4;    % Initial and Final Heading [rad]
+yo = 0; yf=pi();    % Initial and Final Heading [rad]
 drag_coeff=16.91; % Drag Coefficients
 switch_case = 0; % Binary Switch for Feed Forward. [0,1]
-sim_time = 20; % Simulation Time
+% sim_time = 20; % Simulation Time
 
 
 %% 
 %
 
-Kpy=1;Kiy=1;Kdy=1;
+Kpy=.1;Kiy=.1;Kdy=.1;
+% Kps=1;Kis=1;Kds=1;
 sim('EXP_Controller_YAW')
 
 rosshutdown
