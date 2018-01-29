11dd0d858d3c4c0f8a8b5a5726b8933bd5fed909
Schnieders, Joseph A <jaschnie@nps.edu>
2018-01-29 13:56:28 -0800

    Update 29JAN - Tank Testing Day 1

--- Experimental_Impl.m
+++ Experimental_Impl.m
@@ -26,7 +26,7 @@ table_data = data(end:-1:1,2)';
 % Step Inputs
 kfyaw=zeros(50);
 vo = 0.0; vf = 0.0; % Initial and Final Speed [m/s]
-yo = 0; yf=pi/4;    % Initial and Final Heading [rad]
+yo = 0; yf=pi/4;   % Initial and Final Heading [rad]
 drag_coeff=16.91; % Drag Coefficients
 switch_case = 0; % Binary Switch for Feed Forward. [0,1]
 % sim_time = 20; % Simulation Time
@@ -35,8 +35,58 @@ switch_case = 0; % Binary Switch for Feed Forward. [0,1]
 %% 
 %
 
-Kpy=1;Kiy=.01;Kdy=.1;
+% Kpy=30;Kiy=.01;Kdy=5;
 % Kps=1;Kis=1;Kds=1;
-sim('EXP_YAW_TEST')
+% sim('EXP_Controller_YAW')
+
+
+
+% Plotting Variables
+rg = 255;
+C = {[186/rg 18/rg 18/rg],[22/rg 135/rg 0/rg],[252/rg 172/rg 0/rg],[0/rg 29/rg 255/rg]}; % Plot Color Array
+j=1.533;
+
+%% 
+% Gain Matrix for Testing
+% Gain = [1   10  100 1   1   1   1   1   1   1   1   1   1   100 100 100 100 100;
+%         0   0   0   1   .01  .1 10  100 0   0   0   0   0   1   .01 .1  10  100;
+%         0   0   0   0   0   0   0   0   1   .01 .1  10  100 .01 .01 .01 .01 .01];
+%     
+Gain = [42 42 42;
+        .01 .01 .01;
+        10  15  20];
+
+kfhdc = {};
+S = {};
+% rtime={};stime={};os={};sserr={};
+% ss_des = 45;
+figure(1);
+clf;
+
+for i=1:length(Gain)
+      Kpy=Gain(1,i);Kiy=Gain(2,i);Kdy=Gain(3,i);
+        sim('EXP_Controller_YAW')
+      kfhdc{i} = kfhd;
+%       S{i}=stepinfo(surge,time);
+%       rtime{i}=S{1,i}.RiseTime;
+%       stime{i}=S{1,i}.SettlingTime;
+%       os{i}=S{1,i}.Overshoot;
+%       sserr{i}=(ss_des-surge(end));
+      tc{i} = time;
+      j = j-(i/12);
+      plot(time,kfhd,'Color',C{i},'LineWidth',j)
+      hold on
+      grid on
+end
+
+% Ch = [rtime' stime' os' sserr'];
+
+legend(['Kp=',num2str(Gain(1,1)),', Ki=',num2str(Gain(2,1)),', Kd=',num2str(Gain(3,1))], ... 
+    ['Kp=',num2str(Gain(1,2)),', Ki=',num2str(Gain(2,2)),', Kd=',num2str(Gain(3,2))], ... 
+    ['Kp=',num2str(Gain(1,3)),', Ki=',num2str(Gain(2,3)),', Kd=',num2str(Gain(3,3))], ... 
+    'Location','se')
+xlabel('Time [s]')
+ylabel('Kingfisher Heading [deg]')
+title('Yaw Controller Testing - 0-45[deg] Step Input at t=40s')
 
 rosshutdown
